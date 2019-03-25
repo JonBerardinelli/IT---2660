4259bc0ddecc92e8770d06d3ee86591c56b3cf9d
Jon <Jon@DESKTOP-FCVRNAG>
2019-03-24 22:33:51 -0400

    Changed paths for Lab 2

--- Assignment2/2660_Assignment2_20-21/src/CollegeStudentListing.java
+++ Assignment2/2660_Assignment2_20-21/src/CollegeStudentListing.java
@@ -1,41 +1,51 @@
 import javax.swing.JOptionPane;
 
 public class CollegeStudentListing {
-	public class StudentListings {
-		private String name;
-		private String ID;
-		private String GPA;
+	private String name;
+	private String ID;
+	private String GPA;
+	private String paneSelect;
+	
+	CollegeStudentListing(){
 		
-		StudentListings(){
-			
-		}
-		
-		public StudentListings(String n, String i, String g) {
-			name = n;
-			ID = i;
-			GPA = g;
-		}
-		
-		public String toString() {
-			return ("Student Name: " + name + 
-					"\nStudent ID: " + ID + 
-					"\nStudent GPA: " + GPA + "\n" );
-		}
-		
-		public StudentListings deepCopy() {
-			StudentListings clone = new StudentListings(name, ID, GPA);
-			return clone;
-		}
-		
-		public int compareTo(String targetKey) {
-			return(name.compareTo(targetKey));
-		}
-		
-		public void input() {
-			name = JOptionPane.showInputDialog("Enter a name");
-			ID = JOptionPane.showInputDialog("Enter an ID");
-			GPA = JOptionPane.showInputDialog("Enter a GPA");
-			
-		}
 	}
+	
+	public CollegeStudentListing(String n, String i, String g) {
+		name = n;
+		ID = i;
+		GPA = g;
+	}
+	
+	public String toString() {
+		return ("Student Name: " + name + 
+				"\nStudent ID: " + ID + 
+				"\nStudent GPA: " + GPA + "\n" );
+	}
+	
+	public CollegeStudentListing deepCopy() {
+		CollegeStudentListing clone = new CollegeStudentListing(name, ID, GPA);
+		return clone;
+	}
+	
+	public int compareTo(String targetKey) {
+		return(name.compareTo(targetKey));
+	}
+	
+	public void input() {
+		name = JOptionPane.showInputDialog("Enter a name");
+		ID = JOptionPane.showInputDialog("Enter an ID");
+		GPA = JOptionPane.showInputDialog("Enter a GPA");		
+	}
+	public int SelectPane() {
+		paneSelect = JOptionPane.showInputDialog("Choose a number to decide the next operation:" + 
+									"\n\t1: Insert a new student's info" + 
+									"\n\t2: Fetch and output a student's info" + 
+									"\n\t3: Delete a student's info" + 
+									"\n\t4: Update a student's info" + 
+									"\n\t5: Output all students' info" + 
+									"\n\t6: Exit Program");
+		int selectNum = Integer.parseInt(paneSelect);
+		return selectNum;
+	}
+
 }
