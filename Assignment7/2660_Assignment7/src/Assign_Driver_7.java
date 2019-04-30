fe64f25fbb7b0e6f5547c7822746416fcf0b0138
Jon <Jon@DESKTOP-FCVRNAG>
2019-04-30 17:01:17 -0400

    Removed unused code

--- Assignment7/2660_Assignment7/src/Assign_Driver_7.java
+++ Assignment7/2660_Assignment7/src/Assign_Driver_7.java
@@ -9,41 +9,6 @@ public class Assign_Driver_7 {
 		
 		tree.InitialStudentEntry(student,tree);
 		tree.UserFuntionSelection(student,tree);
-/*        
-        CollegeStudentListing student1 = new CollegeStudentListing("Jeff", "012", "4.5");
-        CollegeStudentListing student2 = new CollegeStudentListing("Junna", "123", "3.0");
-        CollegeStudentListing student3 = new CollegeStudentListing("Meina", "234", "3.0");
-        CollegeStudentListing student4 = new CollegeStudentListing("Chikako", "345", "2.1");
-        CollegeStudentListing student5 = new CollegeStudentListing("Vijitra", "456", "3.6");
-        CollegeStudentListing student6 = new CollegeStudentListing("Huiyu", "567", "5.4");
-        CollegeStudentListing student7 = new CollegeStudentListing("Brooke", "678", "0.3");
-        CollegeStudentListing student8 = new CollegeStudentListing("Zooey", "789", "1.1");
-        CollegeStudentListing student9 = new CollegeStudentListing("Sombra", "890", "0.1");
-        
-        CollegeStudentListing student10 = new CollegeStudentListing("Hanzo","001", "6.7");
-        CollegeStudentListing student11 = new CollegeStudentListing("Genji", "002", "6.4");
-        
-        tree.insert(student1);
-        tree.insert(student2);
-        tree.insert(student3);
-        tree.insert(student4);
-        tree.insert(student5);
-        tree.insert(student6);
-        tree.insert(student7);
-        tree.insert(student8);
-        tree.insert(student9);
-        
-        tree.showAll();
-        
-        tree.update("Sombra", student10);
-        
-        tree.showAll();
-        
-        tree.delete("Jeff");
-        
-        tree.showAll();
-        
-*/
 	}
 
 }
